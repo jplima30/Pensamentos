78488f13245e153bea39638b2cd9819292fdadb5
jplima <jplimadev@gmail.com>
2022-09-13 17:23:15 -0300

    firsCommit

---
+++ Pensamentos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pensamentos
+//
+//  Created by jplima on 13/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
