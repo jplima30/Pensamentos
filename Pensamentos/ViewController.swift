ab25440fea44d6d346ce36f73662ab95ff02abfb
jplima <jplimadev@gmail.com>
2022-09-29 11:53:49 -0300

    Layouts_Screens

--- Pensamentos/ViewController.swift
+++ Pensamentos/ViewController.swift
@@ -1,14 +1,7 @@
-//
-//  ViewController.swift
-//  Pensamentos
-//
-//  Created by jplima on 13/09/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController {   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
